e4c18a6ae26fccd7e185736521bb12cbef6daa0e
perterHUAN <47806541+perterHUAN@users.noreply.github.com>
2024-04-13 21:57:05 +0800

    two pass strategy

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,14 +1,17 @@
 "use client";
 import React from "react";
+import Spinner from "../Spinner/Spinner";
 
 function Counter() {
-  const [count, setCount] = React.useState(0);
+  // first render, we get null value for count,
+  // which states we don't get real value from localstorage.
+  const [count, setCount] = React.useState(null);
 
   React.useEffect(() => {
     const savedCount = window.localStorage.getItem("saved-count");
     if (savedCount) {
       setCount(Number(savedCount));
-    }
+    } else setCount(0);
   }, []);
   React.useEffect(() => {
     window.localStorage.setItem("saved-count", count);
@@ -16,7 +19,7 @@ function Counter() {
 
   return (
     <button className="count-btn" onClick={() => setCount(count + 1)}>
-      Count: {count}
+      Count: {count !== null ? count : <Spinner />}
     </button>
   );
 }
